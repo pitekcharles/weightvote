ef9bc2221f8e5d28d563b00519ca3750c30c107c
Charles Pitek <charles.pitek@gmail.com>
2020-08-13 01:18:08 -0600

    finished up poll model

--- models/poll.js
+++ models/poll.js
@@ -8,6 +8,67 @@ module.exports = function (sequelize, DataTypes) {
             type: DataTypes.STRING,
             allowNull: false
         },
+        optionOneNoResistance: {
+            type: DataTypes.INTEGER
+        },
+        optionOneSomeResistance: {
+            type: DataTypes.INTEGER
+        },
+        optionOneMaximumResistance: {
+            type: DataTypes.INTEGER
+        },
+        optionTwo: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        optionTwoNoResistance: {
+            type: DataTypes.INTEGER
+        },
+        optionTwoSomeResistance: {
+            type: DataTypes.INTEGER
+        },
+        optionTwoMaximumResistance: {
+            type: DataTypes.INTEGER
+        },
+        optionThree: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        optionThreeNoResistance: {
+            type: DataTypes.INTEGER
+        },
+        optionThreeSomeResistance: {
+            type: DataTypes.INTEGER
+        },
+        optionThreeMaximumResistance: {
+            type: DataTypes.INTEGER
+        },
+        optionFour: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        optionFourNoResistance: {
+            type: DataTypes.INTEGER
+        },
+        optionFourSomeResistance: {
+            type: DataTypes.INTEGER
+        },
+        optionFourMaximumResistance: {
+            type: DataTypes.INTEGER
+        },
+        optionFive: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        optionFiveNoResistance: {
+            type: DataTypes.INTEGER
+        },
+        optionFiveSomeResistance: {
+            type: DataTypes.INTEGER
+        },
+        optionFiveMaximumResistance: {
+            type: DataTypes.INTEGER
+        }
 
     })
 
